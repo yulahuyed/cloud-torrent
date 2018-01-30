9847a30b4ddad06b75a01bb05bcf3db84dd48f40
yulahuyed <yulahuyed@vpsorg.top>
2018-01-30 22:09:23 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,7 @@ then
     sed -i "s/gdsecret/${GDRIVE_SECRET}/g" /config/rclone.conf
     sed -i "s/gdid/${GDRIVE_ID}/g" /config/rclone.conf
     sed -i "s/gatoken/${GDRIVE_ACTOKEN}/g" /config/rclone.conf
-    rclone --config /config/rclone.conf
-    rclone mount gdrive: $DLPATH/downloads
+    rclone mount gdrive: $DLPATH/downloads --config /config/rclone.conf
 fi
 
 cloud-torrent
