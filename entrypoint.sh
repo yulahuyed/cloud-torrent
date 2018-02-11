104c7416b9186289c317ae5271694b8c562b2bba
yulahuyed <yulahuyed@vpsorg.top>
2018-02-11 13:45:20 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -31,4 +31,34 @@ END
 rclone mount gdrive:${GDRIVE_PATH} $DLPATH/downloads --allow-other --vfs-cache-mode writes --allow-non-empty --ignore-checksum --ignore-size &
 fi
 
+if [ "${OD_PATH}" ]
+then
+
+if [ ! -f "/etc/davfs2/davfs2.conf" ]
+then 
+touch /etc/davfs2/davfs2.conf
+fi
+chmod 777 /etc/davfs2/davfs2.conf
+echo >> /etc/davfs2/davfs2.conf
+echo >> /etc/davfs2/davfs2.conf
+
+python /config/od.py ${OD_PATH} ${OD_USER} ${OD_PASS} > cookie.txt
+sed -i "s/ //g" cookie.txt
+COOKIE=$(cat cookie.txt)
+DAVFS_CONFIG=$(grep -i "use_locks 0" /etc/davfs2/davfs2.conf)
+if [ "${DAVFS_CONFIG}" == "use_locks 0" ] 
+then
+  echo "continue..."
+else
+  echo "use_locks 0" >> /etc/davfs2/davfs2.conf
+fi
+echo "[$DLPATH/downloads]" >> /etc/davfs2/davfs2.conf
+echo "add_header Cookie ${COOKIE}" >> /etc/davfs2/davfs2.conf
+
+rm cookie.txt
+
+/sbin/mount.davfs ${OD_PATH} $DLPATH/downloads
+
+fi
+
 cloud-torrent
